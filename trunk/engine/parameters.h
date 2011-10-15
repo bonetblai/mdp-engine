432125b42b28d11b17278967cee649027f3a64d6
bonetblai@gmail.com <bonetblai@gmail.com@c4d68754-213e-bca4-7919-aa82295edfc3>
2011-10-15 17:32:35 +0000

    Add evaluation class

--- trunk/engine/parameters.h
+++ trunk/engine/parameters.h
@@ -27,16 +27,18 @@ struct parameters_t {
     struct vi_parameters_t {
         unsigned max_number_iterations_;
         vi_parameters_t() : max_number_iterations_(0) { }
-    };
-    vi_parameters_t vi;
+    } vi;
 
     struct rtdp_parameters_t {
         unsigned bound_;
         unsigned max_number_steps_;
         float epsilon_greedy_;
         rtdp_parameters_t() : bound_(0), max_number_steps_(0), epsilon_greedy_(0) { }
-    };
-    rtdp_parameters_t rtdp;
+    } rtdp;
+
+    struct eval_parameters_t {
+        unsigned max_number_steps_;
+    } eval;
 
     parameters_t() : epsilon_(0) { }
 };
