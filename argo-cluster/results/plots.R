980abe6ab008c3f2e0898ce021776a65728b1a99
bonetblai <bonetblai@gmail.com>
2011-11-22 22:49:12 +0000

    Fix uct parameter and generatio of plots

--- argo-cluster/results/plots.R
+++ argo-cluster/results/plots.R
@@ -5,36 +5,31 @@ makeplots = function (myplot.problem) {
 
     par(lwd=1.5, lty=1)
     #par(mfrow=c(1,3))
-    par(mfrow=c(1,1))
+    par(mfrow=c(2,1))
     colors = 2 + (1:5)
 
-    line_random = data.all[problem==myplot.problem & algorithm=="random", "quality.avg"]
-    line_greedy = data.all[problem==myplot.problem & algorithm=="greedy", "quality.avg"]
-    line_optimal = data.all[problem==myplot.problem & algorithm=="optimal", "quality.avg"]
-    line_rollout = data.all[problem==myplot.problem & algorithm=="rollout" & base=="random", "quality.avg"]
+    #line_random = data.all[problem==myplot.problem & algorithm=="random", "quality.avg"]
+    #line_greedy = data.all[problem==myplot.problem & algorithm=="greedy", "quality.avg"]
+    #line_optimal = data.all[problem==myplot.problem & algorithm=="optimal", "quality.avg"]
+    #line_rollout = data.all[problem==myplot.problem & algorithm=="rollout" & base=="random", "quality.avg"]
 
     #for( myplot.base in c("random", "greedy") ) {
-    for( myplot.base in c("random") ) {
+    for( myplot.base in c("greedy") ) {
         yrange = range(data.all[problem==myplot.problem & base==myplot.base & algorithm!="random", "quality.avg"], na.rm=TRUE)
-        yrange=c(100,200)
+        #yrange=c(100,200)
         #for( myplot.algorithm in c("uct", "ao3", "ao4") ) {
-        for( myplot.algorithm in c("ao3") ) {
-            if( myplot.algorithm == "uct" ) {
-                parameters = c(0,-5,-10,-15,-20) #seq(0,-12,by=-3)
-            } else {
-                #parameters = c(0,.2,.4,.6,.8,1) #seq(0,-12,by=-3)
-                parameters = c(.5) #seq(0,-12,by=-3)
-            }
+        for( myplot.algorithm in c("uct", "ao3") ) {
+            parameters = unique(data.all[algorithm==myplot.algorithm & base==myplot.base, c("par")])
     
-            for( i in 1:1 ) {
+            for( i in 1:length(parameters) ) {
                 # extract data
                 p = parameters[i]
                 data.plot = data.all[problem==myplot.problem &
-                                     depth==100 &
+                                     #depth==100 &
                                      algorithm==myplot.algorithm &
                                      base==myplot.base &
-                                     par==p,
-                                     c("width", "quality.avg", "time")]
+                                     data.all$par==p,
+                                     c("width", "quality.avg", "quality.stdev", "time")]
                 data.plot = data.plot[order(data.plot$width),]
                 par(col=colors[i])
                 if( i == 1 ) {
@@ -43,16 +38,15 @@ makeplots = function (myplot.problem) {
                     lines(log(data.plot$width), data.plot$quality.avg, type='l')
                 }
             }
-    
             # horizontal lines
-            par(col=1, lty=2, lwd=0.5)
-            abline(h=line_random)
+            #par(col=1, lty=2, lwd=0.5)
+            #abline(h=line_random)
             #text(x=190,y=.25+line_random, labels=c("random"))
-            abline(h=line_greedy)
+            #abline(h=line_greedy)
             #text(x=190,y=.25+line_greedy, labels=c("greedy"))
-            abline(h=line_optimal)
+            #abline(h=line_optimal)
             #text(x=190,y=.25+line_optimal, labels=c("optimal"))
-            par(lty=1, lwd=1.5)
+            #par(lty=1, lwd=1.5)
     
             # title
             title(paste(myplot.problem, ":", myplot.algorithm, "w/", myplot.base))
