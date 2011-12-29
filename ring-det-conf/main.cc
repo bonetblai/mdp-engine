839857fea542525eeb85ab424bd5331b72f5b98b
bonetblai <bonetblai@gmail.com>
2011-12-29 13:43:07 +0000

    Add problem ring-det-conf

---
+++ ring-det-conf/main.cc
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "ring.h"
+
+#include "../evaluation.h"
+
+using namespace std;
+
+void usage(ostream &os) {
+    os << "usage: ring [-a <n>] [-b <n>] [-e <f>] [-f] [-h <n>] [-s <n>] <n>"
+       << endl << endl
+       << "  -a <n>    Algorithm bitmask: 1=vi, 2=slrtdp, 4=ulrtdp, 8=blrtdp, 16=ilao, 32=plain-check, 64=elrtdp, 128=hdp-i, 256=hdp, 512=ldfs+, 1024=ldfs."
+       << endl
+       << "  -b <n>    Visits bound for blrtdp. Default: inf."
+       << endl
+       << "  -e <f>    Epsilon. Default: 0."
+       << endl
+       << "  -f        Formatted output."
+       << endl
+       << "  -h <n>    Heuristics: 0=zero, 1=minmin. Default: 0."
+       << endl
+       << "  -s <n>    Random seed. Default: 0."
+       << endl
+       << "  <n>       Number of windows."
+       << endl << endl;
+}
+
+int main(int argc, const char **argv) {
+    int dim = 0;
+    unsigned bitmap = 0;
+    int h = 0;
+    bool formatted = false;
+
+    string base_name;
+    string policy_type;
+    Evaluation::parameters_t par;
+
+    cout << fixed;
+    Algorithm::parameters_t parameters;
+
+    // parse arguments
+    ++argv;
+    --argc;
+    while( argc > 1 ) {
+        if( **argv != '-' ) break;
+        switch( (*argv)[1] ) {
+            case 'a':
+                bitmap = strtoul(argv[1], 0, 0);
+                argv += 2;
+                argc -= 2;
+                break;
+            case 'b':
+                parameters.rtdp.bound_ = strtol(argv[1], 0, 0);
+                argv += 2;
+                argc -= 2;
+                break;
+            case 'c':
+                shortcut_cost = strtol(argv[1], 0, 0);
+                argv += 2;
+                argc -= 2;
+                break;
+            case 'e':
+                parameters.epsilon_ = strtod(argv[1], 0);
+                argv += 2;
+                argc -= 2;
+                break;
+            case 'f':
+                formatted = true;
+                ++argv;
+                --argc;
+                break;
+            case 'h':
+                h = strtol(argv[1], 0, 0);
+                argv += 2;
+                argc -= 2;
+                break;
+            case 's':
+                parameters.seed_ = strtoul(argv[1], 0, 0);
+                argv += 2;
+                argc -= 2;
+                break;
+            default:
+                usage(cout);
+                exit(-1);
+        }
+    }
+
+    CTP::graph_t graph(false, shortcut_cost);
+    if( argc >= 3 ) {
+        dim = strtoul(argv[0], 0, 0);
+        base_name = argv[1];
+        policy_type = argv[2];
+        if( argc >= 4 ) par.width_ = strtoul(argv[3], 0, 0);
+        if( argc >= 5 ) par.depth_ = strtoul(argv[4], 0, 0);
+        if( argc >= 6 ) par.par1_ = strtod(argv[5], 0);
+        if( argc >= 7 ) par.par2_ = strtoul(argv[6], 0, 0);
+    } else {
+        usage(cout);
+        exit(-1);
+    }
+
+    // build problem instances
+    cout << "seed=" << parameters.seed_ << endl;
+    Random::seeds(parameters.seed_);
+    //state_t::initialize(graph, false, (int)5e5);
+    //problem_t problem(graph, false, (int)5e5);
+
+    // create heuristic
+    Heuristic::heuristic_t<state_t> *heuristic = 0;
+    if( h == 1 ) {
+        //heuristic = new min_min_t;
+    }
+
+    // solve problem with algorithms
+    vector<Dispatcher::result_t<state_t> > results;
+    Dispatcher::solve(problem, heuristic, problem.init(), bitmap, parameters, results);
+
+    // print results
+    if( !results.empty() ) {
+        if( formatted ) Dispatcher::print_result<state_t>(cout, 0);
+        for( unsigned i = 0; i < results.size(); ++i ) {
+            Dispatcher::print_result(cout, &results[i]);
+        }
+    }
+
+    // evaluate policies
+    vector<pair<const Policy::policy_t<state_t>*, string> > bases;
+
+    // fill base policies
+    const Problem::hash_t<state_t> *hash = results.empty() ? 0 : results[0].hash_;
+    if( hash != 0 ) {
+        Policy::hash_policy_t<state_t> optimal(*hash);
+        bases.push_back(make_pair(optimal.clone(), "optimal"));
+    }
+    if( heuristic != 0 ) {
+        Policy::greedy_t<state_t> greedy(problem, *heuristic);
+        bases.push_back(make_pair(greedy.clone(), "greedy"));
+    }
+    Policy::random_t<state_t> random(problem);
+    bases.push_back(make_pair(&random, "random"));
+
+    // evaluate
+    pair<const Policy::policy_t<state_t>*, std::string> policy = Evaluation::select_policy(base_name, policy_type, bases, par);
+    pair<pair<float, float>, float> eval = Evaluation::evaluate_policy(*policy.first, par, true);
+    cout << policy.second
+         << "= " << setprecision(5) << eval.first.first
+         << " " << eval.first.second
+         << setprecision(2) << " ( " << eval.second << " secs)" << endl;
+
+    // free resources
+    delete policy.first;
+    for( unsigned i = 0; i < results.size(); ++i ) {
+        delete results[i].hash_;
+    }
+    delete heuristic;
+
+    exit(0);
+}
+
