9f6f7f9330e3a77c7b09db7b1b6447994a2e5777
bonetblai <bonetblai@c4d68754-213e-bca4-7919-aa82295edfc3>
2012-03-30 18:22:54 +0000

    Adjusted heuristic_t to include ptr to problem

--- engine/heuristic.h
+++ engine/heuristic.h
@@ -39,9 +39,13 @@ namespace Algorithm {
 namespace Heuristic {
 
 template<typename T> class heuristic_t {
+  protected:
+    const Problem::problem_t<T> *problem_;
+
   public:
-    heuristic_t() { }
+    heuristic_t(const Problem::problem_t<T> *problem = 0) : problem_(problem) { }
     virtual ~heuristic_t() { }
+    const Problem::problem_t<T>& problem() const { return *problem_; }
     virtual float value(const T &s) const = 0;
     virtual void reset_stats() const = 0;
     virtual float setup_time() const = 0;
@@ -53,19 +57,18 @@ template<typename T> class heuristic_t {
 
 template<typename T> class min_min_heuristic_t : public heuristic_t<T> {
   protected:
-    const Problem::problem_t<T> &problem_;
     mutable Problem::min_hash_t<T> hash_;
     mutable float time_;
 
   public:
     min_min_heuristic_t(const Problem::problem_t<T> &problem)
-      : problem_(problem), hash_(problem), time_(0) {
+      : heuristic_t<T>(&problem), hash_(problem), time_(0) {
 
       Algorithm::parameters_t parameters;
       parameters.vi.max_number_iterations_ = std::numeric_limits<unsigned>::max();
 
       float start_time = Utils::read_time_in_seconds();
-      Algorithm::value_iteration<T>(problem_, problem.init(), hash_, parameters);
+      Algorithm::value_iteration<T>(problem(), problem.init(), hash_, parameters);
       float end_time = Utils::read_time_in_seconds();
       time_ = end_time - start_time;
     } 
@@ -78,48 +81,6 @@ template<typename T> class min_min_heuristic_t : public heuristic_t<T> {
     virtual size_t size() const { return hash_.size(); }
     virtual void dump(std::ostream &os) const { hash_.dump(os); }
 };
-
-#if 0
-template<typename T> class hdp_heuristic_t : public heuristic_t<T> {
-  protected:
-    const Problem::problem_t<T> &problem_;
-    mutable Problem::hash_t<T> hash_;
-    float epsilon_;
-    size_t kappa_;
-    mutable float time_;
-
-  public:
-    hdp_heuristic_t(const Problem::problem_t<T> &problem, float epsilon = 0, size_t kappa = 0)
-      : problem_(problem), epsilon_(epsilon), kappa_(kappa), time_(0) {
-    }
-    virtual ~hdp_heuristic_t() { }
-
-    virtual float value(const T &s) const {
-        std::list<Hash::data_t*> visited;
-        std::list<Hash::data_t*> stack;
-        float start_time = Utils::read_time_in_seconds();
-        Hash::data_t *dptr = hash_.data_ptr(s);
-        while( !dptr->solved() ) {
-            size_t index = 0;
-            Algorithm::lenforce(problem_, hash_, s, dptr, epsilon_, index, kappa_, stack, visited);
-            assert(stack.empty());
-            while( !visited.empty() ) {
-                visited.front()->unmark();
-                assert(visited.front()->scc_low() == std::numeric_limits<unsigned>::max());
-                assert(visited.front()->scc_idx() == std::numeric_limits<unsigned>::max());
-                visited.pop_front();
-            }
-        }
-        float end_time = Utils::read_time_in_seconds();
-        time_ += end_time - start_time;
-        return dptr->value();
-    }
-    virtual float setup_time() const { return 0; }
-    virtual float eval_time() const { return time_; }
-    virtual size_t size() const { return hash_.size(); }
-    virtual void dump(std::ostream &os) const { hash_.dump(os); }
-};
-#endif
 
 template<typename T> class hash_heuristic_t : public heuristic_t<T> {
   protected:
