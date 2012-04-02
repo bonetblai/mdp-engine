9070f55ede798e86bba7961381f6ce0d875b40c7
bonetblai <bonetblai@c4d68754-213e-bca4-7919-aa82295edfc3>
2012-04-02 23:22:22 +0000

    Working on lrtdp

--- engine/online_rtdp.h
+++ engine/online_rtdp.h
@@ -114,6 +114,11 @@ template<typename T> class hash_table_t :
         return dptr;
     }
 
+    data_t* get_data_ptr(const node_ref_t<T> &node) {
+        data_t *dptr = data_ptr(node);
+        return dptr == 0 ? insert(node) : dptr;
+    }
+
     void print(std::ostream &os) const {
         for( const_iterator it = begin(); it != end(); ++it ) {
             os << "(" << it->first.first << "," << it->first.second << ")" << std::endl;
@@ -138,24 +143,32 @@ template<typename T> class finite_horizon_lrtdp_t : public policy_t<T> {
 
     // CHECK: heuristic_ is not used!!!!
   public:
-    finite_horizon_lrtdp_t(const Heuristic::heuristic_t<T> &heuristic,
+    finite_horizon_lrtdp_t(const Problem::problem_t<T> &problem,
+                           const Heuristic::heuristic_t<T> &heuristic,
                            unsigned horizon, unsigned max_trials,
                            bool labeling, bool random_ties)
-      : policy_t<T>(heuristic.problem()), heuristic_(heuristic),
+      : policy_t<T>(problem), heuristic_(heuristic),
         horizon_(horizon), max_trials_(max_trials),
-        labeling_(labeling), random_ties_(random_ties) { assert(0); }
+        labeling_(labeling), random_ties_(random_ties) {
+        std::cout << "finite_horizon_lrtdp_t: hola" << std::endl;
+    }
     virtual ~finite_horizon_lrtdp_t() { }
 
     virtual Problem::action_t operator()(const T &s) const {
+        //std::cout << "hola0" << std::endl;
         node_ref_t<T> root(s, 0);
-        for( unsigned trials = 0; (trials < max_trials_) && !labeled(root); ++trials ) {
-            lrtdp_trial(root);
+        data_t *root_dptr = table_.get_data_ptr(root);
+        //std::cout << "hola1: max_trials=" << max_trials_ << std::endl;
+        for( unsigned trials = 0; (trials < max_trials_) && !labeled(root_dptr); ++trials ) {
+            //std::cout << "trial=" << trials << std::endl;
+            lrtdp_trial(root, root_dptr);
         }
+        //std::cout << "hola2" << std::endl;
         return best_action(root, random_ties_);
     }
 
     virtual const policy_t<T>* clone() const {
-        return new finite_horizon_lrtdp_t(heuristic_, horizon_, max_trials_, labeling_, random_ties_);
+        return new finite_horizon_lrtdp_t(policy_t<T>::problem(), heuristic_, horizon_, max_trials_, labeling_, random_ties_);
     }
 
     virtual void print_stats(std::ostream &os) const {
@@ -196,6 +209,7 @@ template<typename T> class finite_horizon_lrtdp_t : public policy_t<T> {
         bool labeled_children = true;
         std::vector<std::pair<T, float> > outcomes;
         policy_t<T>::problem().next(node.state(), a, outcomes);
+        //std::cout << "outcomes: sz=" << outcomes.size() << std::endl;
         for( int i = 0, isz = outcomes.size(); i < isz; ++i ) {
             const T &state = outcomes[i].first;
             float prob = outcomes[i].second;
@@ -203,7 +217,9 @@ template<typename T> class finite_horizon_lrtdp_t : public policy_t<T> {
             std::pair<float, bool> p = value(next_node);
             qvalue += prob * p.first;
             labeled_children = labeled_children && p.second;
+            //std::cout << "  p=" << prob << ", value=" << p.first << std::endl;
         }
+        //std::cout << "qvalue before cost = " << qvalue << std::endl;
         qvalue += policy_t<T>::problem().cost(node.state(), a);
         return std::make_pair(qvalue, labeled_children);
     }
@@ -215,86 +231,127 @@ template<typename T> class finite_horizon_lrtdp_t : public policy_t<T> {
         for( Problem::action_t a = 0; a < nactions; ++a ) {
             if( policy_t<T>::problem().applicable(node.state(), a) ) {
                 std::pair<float, bool> p = QValue(node, a);
-                if( p.first < best_value ) {
+                if( (best_action == Problem::noop) || (p.first < best_value) ) {
                     best_value = p.first;
                     best_action = a;
+                    //std::cout << "BEST=" << p.first << std::endl;
                 }
             }
         }
+        //if( best_action == Problem::noop ) std::cout << "state=" << node.state() << std::endl;
         assert(best_action != Problem::noop);
         return std::make_pair(best_value, best_action);
     }
 
     std::pair<float, bool> value(const node_ref_t<T> &node) const {
+        //std::cout << "value: value=" << std::flush;
         const data_t *dptr = table_.data_ptr(node);
         if( dptr != 0 ) {
+            //std::cout << dptr->value_ << std::endl;
             return std::make_pair(dptr->value_, dptr->labeled_);
         } else {
-            return std::make_pair(0, false);
+            float hvalue = heuristic_.value(node.state());
+            return std::make_pair(hvalue, false);
         }
+    }
+
+    void update_value(data_t *dptr, float value) const {
+        assert(dptr != 0);
+        //std::cout << "update_value: old_value=" << dptr->value_ << ", new_value=" << value << std::endl;
+        dptr->value_ = value;
     }
 
     void update_value(const node_ref_t<T> &node, float value) const {
         data_t *dptr = table_.data_ptr(node);
         if( dptr == 0 ) dptr = table_.insert(node);
-        dptr->value_ = value;
+        update_value(dptr, value);
+    }
+
+    bool labeled(const data_t *dptr) const {
+        assert(dptr != 0);
+        return labeling_ ? false : dptr->labeled_;
     }
 
     bool labeled(const node_ref_t<T> &node) const {
         if( labeling_ ) {
-            const data_t *dptr = table_.data_ptr(node);
-            return dptr->labeled_;
-        } else {
             return false;
+        } else {
+            data_t *dptr = table_.data_ptr(node);
+            return dptr == 0 ? false : labeled(dptr);
         }
     }
-    //bool labeled(const node_t<T> &node) const {
-    //    return labeled(node_ref_t<T>(node));
-    //}
 
-    bool label(const node_ref_t<T> &node) const {
+    bool label(const node_ref_t<T> &node, data_t *dptr) const {
+        std::cout << "begin labeling" << std::endl;
         bool labeled = true;
-        std::pair<float, Problem::action_t> p = bestQValue(node);
-
-        data_t *dptr = table_.data_ptr(node);
+        if( dead_end(node) ) {
+            dptr->value_ = 10e3; // XXXXXXX
+        } else if( terminal(node) ) {
+            float value = heuristic_.value(node.state());
+            //std::cout << "terminal value=" << value << std::endl;
+            //std::cout << node.state() << std::endl;
+            dptr->value_ = value;
+        } else {
+            labeled = false;
+            //std::pair<float, Problem::action_t> p = bestQValue(node);
+            //std::cout << "HOLA" << std::endl;
+        }
         dptr->labeled_ = labeled;
+        std::cout << "end labeling" << std::endl;
         return labeled;
     }
-    //bool label(const node_t<T> &node) const {
-    //    return label(node_ref_t<T>(node));
-    //}
 
     bool terminal(const node_ref_t<T> &node) const {
         return (node.depth() >= horizon_) || policy_t<T>::problem().terminal(node.state());
     }
-    //bool terminal(const node_t<T> &node) const {
-    //    return terminal(node_ref_t<T>(node));
-    //}
 
-    void lrtdp_trial(const node_ref_t<T> &root) const {
-        std::vector<T> visited;
+    bool dead_end(const node_ref_t<T> &node) const {
+        //std::cout << "dead_end: status=" << (policy_t<T>::problem().dead_end(node.state()) ? 1 : 0) << std::endl;
+        return policy_t<T>::problem().dead_end(node.state());
+    }
+
+    void lrtdp_trial(const node_ref_t<T> &root, data_t *root_dptr) const {
+        std::vector<std::pair<T, data_t*> > visited;
         if( labeling_ ) {
             visited.reserve(horizon_);
-            visited.push_back(root.state());
+            visited.push_back(std::make_pair(root.state(), root_dptr));
         }
 
         // lrtdp trial
         node_t<T> node(root);
-        while( !labeled(node) && !terminal(node) ) {
+        bool node_is_dead_end = dead_end(node);
+        data_t *dptr = table_.get_data_ptr(node);
+        while( !labeled(dptr) && !terminal(node) && !node_is_dead_end ) {
+            //std::cout << "lrtdp_trial: step" << std::endl;
             std::pair<float, Problem::action_t> p = bestQValue(node);
             Problem::action_t best_action = p.second;
             update_value(node, p.first);
             node.set_state(policy_t<T>::problem().sample(node.state(), best_action).first);
             node.set_depth(1 + node.depth());
-            if( labeling_ ) visited.push_back(node.state());
+            if( labeling_ ) visited.push_back(std::make_pair(node.state(), dptr));
+            node_is_dead_end = dead_end(node);
+            dptr = table_.get_data_ptr(node);
+        }
+        //std::cout << "END TRIAL: terminal=" << (terminal(node) ? 1 : 0) << std::endl;
+
+        if( node_is_dead_end ) {
+            //std::cout << "NODE IS DEAD END" << std::endl;
+        }
+
+        if( terminal(node) || node_is_dead_end ) {
+            dptr->value_ = 0;
         }
 
         // try labeling nodes in reverse visited order
+        //std::cout << "BEGIN LABELING: sz=" << visited.size() << std::endl;
         for( unsigned depth = visited.size(); depth > 0; --depth ) {
-            node_t<T> node(visited[depth - 1], depth - 1);
-            bool labeled = label(node);
+            //std::cout << "depth=" << depth << std::endl;
+            node_t<T> node(visited[depth - 1].first, depth - 1);
+            data_t  *dptr = visited[depth - 1].second;
+            bool labeled = label(node, dptr);
             if( !labeled ) break;
         }
+        //std::cout << "END LABELING" << std::endl;
     }
 
 };
@@ -302,12 +359,13 @@ template<typename T> class finite_horizon_lrtdp_t : public policy_t<T> {
 }; // namespace RTDP
 
 template<typename T>
-inline const policy_t<T>* make_finite_horizon_lrtdp(const Heuristic::heuristic_t<T> &heuristic,
+inline const policy_t<T>* make_finite_horizon_lrtdp(const Problem::problem_t<T> &problem,
+                                                    const Heuristic::heuristic_t<T> &heuristic,
                                                     unsigned horizon,
                                                     unsigned max_trials,
                                                     bool labeling,
                                                     bool random_ties) {
-    return new RTDP::finite_horizon_lrtdp_t<T>(heuristic, horizon, max_trials, labeling, random_ties);
+    return new RTDP::finite_horizon_lrtdp_t<T>(problem, heuristic, horizon, max_trials, false, random_ties);
 }
 
 }; // namespace Policy
