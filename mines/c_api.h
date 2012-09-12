7bbebd66ef9fb63f43bc9f6e867aae6e127b517c
bonetblai <bonetblai@c4d68754-213e-bca4-7919-aa82295edfc3>
2012-09-12 19:16:13 +0000

    working

--- mines/c_api.h
+++
@@ -1,6 +0,0 @@
-void agent_initialize(int rows, int cols, int nmines);
-int agent_get_action();
-int agent_is_flag_action(int action);
-int agent_get_cell(int action);
-void agent_update_state(int flag, int cell, int obs);
-
