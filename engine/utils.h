341eedb0ae88cb3b6d08bafe4a8cb5f8f7dbe19d
Blai Bonet <blaibonet@imac27.local>
2016-04-11 06:21:24 -0430

    add jenkins hash function

--- engine/utils.h
+++ engine/utils.h
@@ -136,10 +136,27 @@ inline void print_bits(std::ostream &os, unsigned bitmap, int n) {
     os << std::flush;
 }
 
-template<typename T> inline uint32_t jenkins_one_at_a_time_hash(const std::vector<T> &key) {
+template<typename T> inline uint32_t jenkins_one_at_a_time_hash(const T *key, size_t size) {
     uint32_t hash = 0;
-    for( size_t i = 0; i < key.size(); ++i ) {
+    for( size_t i = 0; i < size; ++i ) {
         hash += key[i].hash();
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+    return hash;
+}
+
+template<typename T> inline uint32_t jenkins_one_at_a_time_hash(const std::vector<T> &key) {
+    return jenkins_one_at_a_time_hash(&key[0], key.size());
+}
+
+template<> inline uint32_t jenkins_one_at_a_time_hash<int>(const int *key, size_t size) {
+    uint32_t hash = 0;
+    for( size_t i = 0; i < size; ++i ) {
+        hash += key[i];
         hash += (hash << 10);
         hash ^= (hash >> 6);
     }
